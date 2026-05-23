bd6f5012b4bbbe5b1e8ea774125f53cbc4b7b345
mfaruqi35 <muhammad.faruqi35@gmail.com>
2026-05-23 11:19:01 +0700

    fix: fix testing_tts import path

--- utils/testing_tts.py
+++ utils/testing_tts.py
@@ -1,5 +1,12 @@
+import sys
+import os
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from app.tts import transcribe_text_to_speech
 import os
+
+"""
+Py file for debugging
+"""
 
 text = "upacara mencari cari carikan percikan secercah ceria canda cinta"
 print(f"[TEXT] {text}")
